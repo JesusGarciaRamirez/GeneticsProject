79a0cf6e468e771ec556183213a2e738e6fd195c
jeux19 <jesgarram19@gmail.com>
2019-10-30 18:11:17 +0100

    Cargar datos del dataset i

---
+++ template/load_data.m
@@ -0,0 +1,11 @@
+function x,y,NVAR = load_data(index)
+%myFun - Description
+%
+% Syntax: output = myFun(input)
+%
+% Long description
+%Get data from  dataset(Index) 
+data = load(['datasets/' datasets{index}]);
+x=data(:,1)/max([data(:,1);data(:,2)]);y=data(:,2)/max([data(:,1);data(:,2)]);
+NVAR=size(data,1);
+end
