be6eed103e8faddf87491cdecd9e1a672996313d
jeux19 <jesgarram19@gmail.com>
2019-11-29 19:21:30 +0100

    cambios

--- template/Results/script_tables.py
+++ template/Results/script_tables.py
@@ -5,45 +5,43 @@ import argparse
 def get_parser():
     parser = argparse.ArgumentParser(description="Tables ranking")
     parser.add_argument(
-        "table_name",
+        "--table_name",
         help="name to table to process",
     )
     return parser
 
-def process_table(table_name):
+
+def process_table_basic(table_name):
+    #Path to table
     table_folder=os.getcwd()
     table_path=os.path.join(table_folder,table_name)
     #opening table
     assert os.path.isfile(table_path) , "Path not correctly defined"
     res_table=pd.read_csv(table_path)
-    # if ("Eff_Var" in res_table.columns):
-    #     res_table=res_table.drop(columns="Eff_Var")
-    
 
+    #Cleaning
     res_table=res_table.drop(columns={"Eff_2","Fit_var"})
-    res_table["Av_Best"]=res_table["Av_Best"].apply(lambda x: 1/x )
-    res_table["Peak_Best"]=res_table["Peak_Best"].apply(lambda x: 1/x )
+    res_table["Av_Best"]=res_table["Av_Best"].apply(lambda x: 1/x)
+    res_table["Peak_Best"]=res_table["Peak_Best"].apply(lambda x: 1/x)
     #Normalizing columns
-    metric_list=["Av_Best","Eff_1","Peak_Best"]
+    metric_list=["Av_Best","Eff_1"]
     norm_table=normalise_metrics(metric_list,res_table)
-    # norm_table.head()
 
     # ##Getting final metric(average of metrics) and ranking
-    # norm_table["sum"]=norm_table[["Av_Best_unit","Fit_var_unit","Eff_unit"]].sum(axis=1)
-    # final_table=norm_table.sort_values("sum",ascending=False)
-    
-    # final_table=final_table.drop(columns='Unnamed: 0')  ##we have to do this efficiently !
+    norm_table['M'] = norm_table.apply(lambda row: (0.5*(row["Av_Best_unit"]+row["Eff_1_unit"])), axis=1)
+    norm_table.sort_values("M",ascending=False)[:10] #Get the best 10 values
     #Saving table
-    final_table_name=get_tablename(table_name)
+    final_table_name=get_new_tablename(table_name,"Fittest")
     table_path=os.path.join(table_folder,final_table_name)
     norm_table.to_csv(table_path)
     print("Correctly saved table  {0}".format(table_name))
 
 
-
-
 if __name__ == "__main__":
 
     args = get_parser().parse_args()
-    process_table(args.table_name)
-    pass
+    process_table_basic(args.table_name)
+
+    # if args.type==1
+    #     process_table
+    # pass
--- template/Results/utils_tables.py
+++ template/Results/utils_tables.py
@@ -3,28 +3,33 @@ import os
 import numpy as np
 
 import seaborn as sns
+from pathlib import Path
 
 # current_path=os.getcwd()
 # tables=[table for table in os.listdir(current_path) if "csv" in table]
 
-def table_processing(table_name,current_path):
-    table_path=os.path.join(current_path,table_name)
-    res_table=pd.read_csv(table_path)
-    res_table=res_table.drop(columns=['NVAR', 'Fit_Var']) #Cleaning
-    # Ordering
-    ordered_table=res_table.sort_values("Av_Best",ascending=False)
-    # Table of the fittest
-    fittest_table=ordered_table[:10]
-    #Getting new table name from "old" table name
-    new_table_path= get_tablename(table_name)
-    fittest_table.to_csv(new_table_path)
+# def table_processing(table_name,current_path):
+#     table_path=os.path.join(current_path,table_name)
+#     res_table=pd.read_csv(table_path)
+#     res_table=res_table.drop(columns=['NVAR', 'Fit_Var']) #Cleaning
+#     # Ordering
+#     ordered_table=res_table.sort_values("Av_Best",ascending=False)
+#     # Table of the fittest
+#     fittest_table=ordered_table[:10]
+#     #Getting new table name from "old" table name
+#     new_table_path= get_tablename(table_name)
+#     fittest_table.to_csv(new_table_path)
+# pass
+
+def get_new_tablename(table_name,process_type):
+    path=Path(table_name)
+    new_path=path.with_name(path.stem+ "_"+process_type +path.suffix)
+    return new_path
 pass
 
-def get_tablename(table_name):
-    name=table_name.split(".")
-    name_cropped=name[0]+"_Fittest."+name[1]
-    return name_cropped
-pass
+
+def rank_tables(table_list,criteria):
+    return
 
 
 def normalise_metrics(metric_list,res_table):
