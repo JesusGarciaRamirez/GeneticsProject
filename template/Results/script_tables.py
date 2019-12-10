7f8474dba2b5f6accc116ec35b2e1c8a708092e9
jeux19 <jesgarram19@gmail.com>
2019-12-10 12:19:56 +0100

    Finetuning Ap3 Analysis

--- template/Results/script_tables.py
+++ template/Results/script_tables.py
@@ -3,10 +3,18 @@ from utils_tables import *
 import argparse
 
 def get_parser():
-    parser = argparse.ArgumentParser(description="Tables ranking")
+    parser = argparse.ArgumentParser(description="Tables processing")
     parser.add_argument(
         "--table_name",
         help="name to table to process",
+    )
+    parser.add_argument(
+        "--process_type",
+        help="type of process",
+    )
+    parser.add_argument(
+        "--par_type",
+        help="Parameter to work with (can be CROSSOVER or MUT)",
     )
     return parser
 
@@ -36,12 +44,57 @@ def process_table_basic(table_name):
     norm_table.to_csv(table_path)
     print("Correctly saved table  {0}".format(table_name))
 
+def get_best_parameters(tuning_table,par_type): #Par_type can be "CROSSOVER" or "MUT"
+    # #Make copy to work with
+    # tuning_copy=tuning_table.copy()
+    #Sorting and selecting 
+    tuning_table=tuning_table.sort_values("Av_Best",ascending=False)
+    best_cross_counts=tuning_table[par_type][:50].value_counts()
+    return best_cross_counts.keys()
+
+##Process Tuning Tables
+
+table_list=[table for table in os.listdir(tuning_folder) if "csv" in table]
+print(table_list)
+tuning_dict={}
+for table in table_list:
+    #open table
+    tuning_table=pd.read_csv(os.path.join(tuning_folder,table))
+    
+    #Av_Best (1/Av)
+    tuning_table["Av_Best"]=tuning_table["Av_Best"].apply(lambda x: 1/x)
+    #Normalize cols
+    metric_list=["Av_Best"]
+    tuning_table=normalise_metrics(metric_list,tuning_table)
+
+    # #Getting best_cross 
+    best_cross=get_best_parameters(tuning_table,"CROSS")
+    tuning_table=tuning_table[tuning_table["CROSS"].isin(best_cross)]
+    #append
+    tuning_dict[table]=tuning_table
+    #clear
+    del tuning_table
+
+
+
 
 if __name__ == "__main__":
 
     args = get_parser().parse_args()
-    process_table_basic(args.table_name)
+    if (args.process_type=="basic"):
+        process_table_basic(args.table_name)
+
+    elif (args.process_type=="tuning"):
+        
+        get_best_parameters(tuning_table,args.par_type)
+        pass
 
     # if args.type==1
     #     process_table
     # pass
+
+
+
+
+
+    
